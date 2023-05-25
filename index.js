2d8920dba4a88b5684330f505dc9e835527f5624
Your Name <kanukaharish@gmail.com>
2023-05-25 06:44:28 +0530

    Connected to the Data Base

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ const express = require('express');
 
 const app = express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
+
+const db = require('./config/mongoose');
+
+app.use(express.static('./assets'));
+// Use express-ejs-layouts (this must be done before routes)
+app.use(expressLayouts);
+
+//Extract styles and Scripts from sub pages into layouts
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 //Use express router
 app.use('/', require('./routes'));
 
